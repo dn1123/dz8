088ce46440d3fcd611ce454e00078810f935927f
dn1123 <112434391+dn1123@users.noreply.github.com>
2022-10-07 20:23:26 +0300

    Add files via upload

---
+++ zad62/Program.cs
@@ -0,0 +1,36 @@
+﻿int[,] matrix = new int[4, 4];
+
+int row = 0;
+int col = 0;
+int count = 1;
+
+
+while (count <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+    matrix[row, col] = count;
+    count++;
+
+    if (row <= col + 1 && row + col < matrix.GetLength(1) - 1)
+        col++;
+    else if (row < col && row + col >= matrix.GetLength(0) - 1)
+        row++;
+    else if (row >= col && row + col > matrix.GetLength(1) - 1)
+        col--;
+    else
+        row--;
+}
+
+Fill2DArray(matrix);
+
+void Fill2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],3}");
+        }
+        Console.WriteLine();
+    }
+}
+
